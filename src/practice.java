0cad942b63fa82dd4184f30b9cdcfef1cc0d18a7
aayush <dhiraayush@ymail.com>
2021-01-07 21:05:36 -0500

    first commit

---
+++ src/practice.java
@@ -0,0 +1,10 @@
+public class practice {
+    public static void main(String[] args) {
+
+        String myName = "Eduardo";
+        String lastName = "D'Avila";
+        int myAge = 45;
+        System.out.println("Hi my name is " + myName+" "+ lastName + " and I am " + myAge + " years old.");
+    }
+}
+
