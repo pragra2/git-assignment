bbb08e2c2aa9d828bbafff5b1c4d7add3820ad79
pragra2 <aayush@pragra.co>
2021-01-08 11:26:24 -0500

    Assignment

--- src/practice.java
+++ src/practice.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class practice {
     public static void main(String[] args) {
-
-        String myName = "Eduardo";
-        String lastName = "D'Avila";
-        int myAge = 45;
-        System.out.println("Hi my name is " + myName+" "+ lastName + " and I am " + myAge + " years old.");
+        List<String> students = new ArrayList<String>();
+        students.add("John");
+        System.out.println(students);
     }
 }
 
