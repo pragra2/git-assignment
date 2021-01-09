b21c6a752f26209c49a9a0c93fbe249d827008b9
Charandeep Deol <csdeol100@gmail.com>
2021-01-09 13:00:16 -0500

    Added Charandeep to the student's list in file practice.java

--- src/practice.java
+++ src/practice.java
@@ -5,6 +5,7 @@ public class practice {
     public static void main(String[] args) {
         List<String> students = new ArrayList<String>();
         students.add("John");
+        students.add("Charandeep");
         System.out.println(students);
     }
 }
